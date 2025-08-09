ab8a77ba6b2167504ae9221528b73e8de0c98064
Nedz-space <0xnedz@gmail.com>
2025-08-09 18:10:22 +0100

    Added 8-index.html, 8-styles.css, 8-script.js

---
+++ headphones/8-script.js
@@ -0,0 +1,7 @@
+// Hamburger toggle logic
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('show');
+});
